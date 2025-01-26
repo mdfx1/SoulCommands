990d03b706e2d7fea829c396d0fddacfb62dc70e
Sandro <sandro.bohmer@outlook.de>
2025-01-26 20:07:33 +0100

    Added Message Class

---
+++ src/main/java/de/elia/utils/Message.java
@@ -0,0 +1,38 @@
+package de.elia.utils;
+
+import de.elia.Main;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Message {
+
+  static MiniMessage miniMessage = MiniMessage.miniMessage();
+
+  public static void mainPrefix(String messageIn, Player playerIn) {
+    Component messageOut = miniMessage.deserialize(messageIn);
+    playerIn.sendMessage(miniMessage.deserialize("<dark_grey>[</dark_grey><gradient:#783D9F:#FF88FD><bold>SoulSMP</bold><dark_grey>]</dark_grey> ").append(messageOut));
+  }
+  public static void mainPrefix(Component componentMessage, Player playerIn) {
+    playerIn.sendMessage(miniMessage.deserialize("<dark_grey>[</dark_grey><gradient:#783D9F:#FF88FD><bold>SoulSMP</bold><dark_grey>]</dark_grey> ").append(componentMessage));
+  }
+  public static void cwPrefix(String messageIn, Player playerIn) {
+    Component messageOut = miniMessage.deserialize(messageIn);
+    playerIn.sendMessage(miniMessage.deserialize("<dark_grey>[</dark_grey><gradient:#307DBB:#69BEFF><bold>CW</bold><dark_grey>]</dark_grey> ").append(messageOut));
+  }
+  public static void errorPrefix(String messageIn, Player playerIn) {
+    Component messageOut = miniMessage.deserialize(messageIn);
+    playerIn.sendMessage(miniMessage.deserialize("<dark_grey>[</dark_grey><gradient:#A01B1B:#E75656><bold>CW</bold><dark_grey>]</dark_grey> ").append(messageOut));
+  }
+  public static void errorPrefix(String messageIn, CommandSender sender) {
+    Component messageOut = miniMessage.deserialize(messageIn);
+    sender.sendMessage(miniMessage.deserialize("<dark_grey>[</dark_grey><gradient:#A01B1B:#E75656><bold>CW</bold><dark_grey>]</dark_grey> ").append(messageOut));
+  }
+
+
+  public static void standard(String messageIn, Player playerIn) {
+    Component messageOut = miniMessage.deserialize(messageIn);
+    playerIn.sendMessage(messageOut);
+  }
+}
